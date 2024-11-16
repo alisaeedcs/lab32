8d3779c8e9975bbb733607a6029fcec7e60ae2b7
Ali Saeed <asaeed5637@gmail.com>
2024-11-15 18:41:36 -0800

    #2 commit

--- main.cpp
+++ main.cpp
@@ -4,7 +4,23 @@
 
 using namespace std;
 
+const int INITIAL = 2;
+
 int main() {
+    srand(time(0));
+
+    deque<Car> carDeque;
+
+    for (int i = 0; i < INITIAL; i++) {
+        carDeque.push_back(Car());
+    }
+
+    cout << "Initial queue"
+
 
     return 0;
+}
+
+void outputDeque(const deque<Car>& carDeque) {
+
 }
