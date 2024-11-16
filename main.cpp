bd04a694fe8a224965799541f8bed5724f367131
Ali Saeed <asaeed5637@gmail.com>
2024-11-15 18:51:39 -0800

    #3 commit

--- main.cpp
+++ main.cpp
@@ -6,6 +6,8 @@ using namespace std;
 
 const int INITIAL = 2;
 
+void outputDeque(deque<Car>&);
+
 int main() {
     srand(time(0));
 
@@ -14,13 +16,32 @@ int main() {
     for (int i = 0; i < INITIAL; i++) {
         carDeque.push_back(Car());
     }
+    cout << "Initial queue:\n";
 
-    cout << "Initial queue"
+    outputDeque(carDeque);
 
+    int count = 0;
+    while (!carDeque.empty()) {
+        count++;
+
+        int num = rand() % 100;
+        
+        //55 percent car leave
+        if (num < 55) {
+            cout << 
+        }
+
+    }
 
     return 0;
 }
 
-void outputDeque(const deque<Car>& carDeque) {
-
+void outputDeque(deque<Car>& carDeque) {
+    if (carDeque.empty()) {
+        cout << "\tEmpty\n";
+    }
+    for (Car& temp : carDeque) {
+        cout << "\t";
+        temp.print();
+    }
 }
